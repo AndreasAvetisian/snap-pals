17a21890932c0f526b4bbf94f8b669d7f5e48850
Andreas Avetisian <t0avan00@students.oamk.fi>
2024-04-07 13:50:35 +0200

    "SignupForm" page and authentication implementation
    Finished implementing "SignupForm" page;
    Connected appwrite to this project. Setted up authentication.

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -2,13 +2,16 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Button } from "@/components/ui/button"
 import { useForm } from "react-hook-form";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { SignupValidation } from "@/lib/validation";
 import { z } from "zod";
+import Loader from "@/components/shared/Loader";
+import { Link } from "react-router-dom";
+import { createUserAccount } from "@/lib/appwrite/api";
 
 const SignupForm = () => {
-  const isLoading = true;
+  const isLoading = false;
 
   const form = useForm<z.infer<typeof SignupValidation>>({
     resolver: zodResolver(SignupValidation),
@@ -20,8 +23,9 @@ const SignupForm = () => {
     },
   })
 
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
-    console.log(values)
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
+    const newUser = await createUserAccount(values);
+    console.log(newUser);
   }
 
   return (
@@ -31,7 +35,7 @@ const SignupForm = () => {
 
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
         <p className="text-light-3 small-medium md:base-regular mt-2">
-          To use SnapPals enter your details
+          To use SnapPals, please enter your details
         </p>
 
         <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
@@ -90,10 +94,30 @@ const SignupForm = () => {
           <Button type="submit" className="shad-button_primary">
             {isLoading ? (
               <div className="flex-center gap-2">
-                Loading...
+                <Loader /> Loading...
               </div>
-            ) : "Sihgn up"}
+            ) : "Sign up"}
           </Button>
+
+          <p className="
+            text-small-regular 
+          text-light-2 
+            text-center 
+            mt-2
+          ">
+            Aldready have an account?
+            <Link 
+              to="/sign-in" 
+              className="
+                text-primary 
+              text-primary-500 
+                text-small-semibold
+                ml-1
+              "
+            >
+              Log in
+            </Link>
+          </p>
         </form>
       </div>
     </Form>
--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <div className="flex-center w-full">
+        <img 
+            src="/assets/icons/loader.svg" 
+            alt="loader"
+            width={24}
+            height={24}
+        />
+    </div>
   )
 }
 
