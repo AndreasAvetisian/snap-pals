6253bcdf446843c6f30a5c15bfdf22dbb2a6a677
Andreas Avetisian <t0avan00@students.oamk.fi>
2024-04-09 16:24:33 +0200

    "EditPost" implementation
    Created "EditPost" page;
    Created "getPostById", "updatePost", and "deletePost" functions in api.ts;
    Created "useGetPostById" query and "useUpdatePost", "useDeletePost" mutations in queriesAndMutations.ts;

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -12,14 +12,23 @@ import { PostValidation } from "@/lib/validation"
 import { Models } from "appwrite"
 import { useUserContext } from "@/context/AuthContext"
 import { useToast } from "../ui/use-toast"
-import { useCreatePost } from "@/lib/react-query/queriesAndMutations"
+import { useCreatePost, useUpdatePost } from "@/lib/react-query/queriesAndMutations"
 
 type PostFormProps = {
     post?: Models.Document;
+    action: 'Create' | 'Update';
 }
 
-const PostForm = ({ post }: PostFormProps) => {
-    const { mutateAsync: createPost, isPending: isLoadingCreate } = useCreatePost();
+const PostForm = ({ post, action }: PostFormProps) => {
+    const { 
+        mutateAsync: createPost, 
+        isPending: isLoadingCreate 
+    } = useCreatePost();
+    const { 
+        mutateAsync: updatePost, 
+        isPending: isLoadingUpdate 
+    } = useUpdatePost();
+
     const { user } = useUserContext();
     const { toast } = useToast();
     const navigate = useNavigate();
@@ -35,6 +44,23 @@ const PostForm = ({ post }: PostFormProps) => {
     });
 
     async function onSubmit(values: z.infer<typeof PostValidation>) {
+        if (post && action === 'Update') {
+            const updatedPost = await updatePost({
+                ...values,
+                postId: post.$id,
+                imageId: post?.imageId,
+                imageUrl: post.imageUrl
+            })
+
+            if (!updatedPost) {
+                toast({
+                    title: 'Please try again'
+                })
+            }
+
+            return navigate(`/post/${post.$id}`)
+        }
+
         const newPost = await createPost({
             ...values,
             userId: user.id,
@@ -134,8 +160,9 @@ const PostForm = ({ post }: PostFormProps) => {
                     <Button 
                         type="submit" 
                         className="shad-button_primary whitespace-nowrap h-12 w-24"
+                        disabled={isLoadingCreate || isLoadingUpdate}
                     >
-                        Submit
+                        {isLoadingCreate || isLoadingUpdate ? 'Loading...' : action}
                     </Button>
                 </div>
             </form>
