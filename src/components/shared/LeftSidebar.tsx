5e40804d233f1a0c8f610e5f3a84801abf22db22
Andreas Avetisian <t0avan00@students.oamk.fi>
2024-04-10 18:33:10 +0200

    "AllUsers" and "Save" pages implementation
    Implemented "AllUsers" and "Save" pages;
    Created "getUsers" function and "useGetUsers" query.

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,20 +1,28 @@
-import { Link, NavLink, useNavigate, useLocation } from 'react-router-dom';
-import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations';
-import { useEffect } from 'react';
-import { useUserContext } from '@/context/AuthContext';
-import { sidebarLinks } from '@/constants';
-import { INavLink } from '@/types';
-import { Button } from '../ui/button';
+import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
+
+import { INavLink } from "@/types";
+import { sidebarLinks } from "@/constants";
+import { Button } from "@/components/ui/button";
+import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
+import { useUserContext, INITIAL_USER } from "@/context/AuthContext";
+import Loader from "./Loader";
 
 const LeftSidebar = () => {
-    const { pathname } = useLocation();
-    const { mutate: signOut, isSuccess } = useSignOutAccount();
     const navigate = useNavigate();
-    const { user } = useUserContext();
+    const { pathname } = useLocation();
+    const { user, setUser, setIsAuthenticated, isLoading } = useUserContext();
 
-    useEffect(() => {
-        if (isSuccess) navigate(0);
-    }, [isSuccess]);
+    const { mutate: signOut } = useSignOutAccount();
+
+    const handleSignOut = async (
+        e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => {
+        e.preventDefault();
+        signOut();
+        setIsAuthenticated(false);
+        setUser(INITIAL_USER);
+        navigate("/sign-in");
+    };
 
     return (
         <nav className="leftsidebar">
@@ -31,54 +39,49 @@ const LeftSidebar = () => {
                     />
                 </Link>
 
-                <Link 
-                    to={`/profile/${user.id}`}
-                    className="flex gap-3 items-center"
-                >
-                    <img
-                        src={user.imageUrl || '/assets/icons/profile-placeholder.svg'}
-                        alt="profile"
-                        className="h-14 w-14 rounded-full"
-                    />
-                    <div className="flex flex-col">
-                        <p className="body-bold">
-                            {user.name}
-                        </p>
-                        <p className="small-regular text-light-3">
-                            @{user.username}
-                        </p>
+                {isLoading || !user.email ? (
+                    <div className="h-14">
+                        <Loader />
                     </div>
-                </Link>
+                ) : (
+                    <Link to={`/profile/${user.id}`} className="flex gap-3 items-center">
+                        <img
+                            src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
+                            alt="profile"
+                            className="h-14 w-14 rounded-full"
+                        />
+                        <div className="flex flex-col">
+                            <p className="body-bold">{user.name}</p>
+                            <p className="small-regular text-light-3">@{user.username}</p>
+                        </div>
+                    </Link>
+                )}
 
                 <ul className="flex flex-col gap-6">
                     {sidebarLinks.map((link: INavLink) => {
                         const isActive = pathname === link.route;
 
                         return (
-                            <li 
-                                className={`
-                                    leftsidebar-link 
-                                    ${isActive && 'bg-primary-500'}
-                                    group
-                                `} 
+                            <li
                                 key={link.label}
+                                className={`leftsidebar-link group ${
+                                    isActive && "bg-primary-500"
+                                }`}
                             >
-                                <NavLink 
+                                <NavLink
                                     to={link.route}
-                                    className="flex gap-4 items-center p-4"
-                                >
+                                    className="flex gap-4 items-center p-4">
                                     <img
                                         src={link.imgURL}
                                         alt={link.label}
-                                        className={`
-                                            group-hover:invert-white 
-                                            ${isActive && 'invert-white'}
-                                        `}
+                                        className={`group-hover:invert-white ${
+                                          isActive && "invert-white"
+                                        }`}
                                     />
                                     {link.label}
                                 </NavLink>
                             </li>
-                        )
+                        );
                     })}
                 </ul>
             </div>
@@ -86,13 +89,12 @@ const LeftSidebar = () => {
             <Button 
                 variant="ghost"
                 className="shad-button_ghost"
-                onClick={() => signOut()}
-            >
+                onClick={handleSignOut}>
                 <img src="/assets/icons/logout.svg" alt="logout" />
                 <p className="small-medium lg:base-medium">Logout</p>
             </Button>
         </nav>
-    )
-}
+    );
+};
 
-export default LeftSidebar
+export default LeftSidebar;
